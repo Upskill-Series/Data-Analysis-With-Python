eddeb04e4ba4cb09ce3966d4b15ffaa2cdfdbbf1
John-Mukhwana <johnbradill67@gmail.com>
2025-03-08 21:42:33 +0300

    Refactor Python scripts: comment out existing code and add condition statements for number evaluation

--- Py_Programs/if_state.py
+++ Py_Programs/if_state.py
@@ -1,6 +1,16 @@
-discount = 0
-amount = int(input("Enter Amount"))
-if amount > 1000:
-    discount = amount*0.10
-print("Discount =", discount)
-print('Net amount', amount-discount)
+# discount = 0
+# amount = int(input("Enter Amount"))
+# if amount > 1000:
+#     discount = amount*0.10
+# print("Discount =", discount)
+# print('Net amount', amount-discount)
+
+a=330
+b=200
+# a and b whci one is greater
+if b>a:
+    print("b is greater than a")
+elif a==b:
+    print("a and b are equal to each other")
+else:
+    print("a is greater than b")
---
+++ Py_Programs/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+
+# Condition statement
+
+number=int(input("Enter your number: "))
+if number>0:
+    print("This number is positve")
+elif number<0:
+    print("This number is negative")
+else:
+    print("zero")
